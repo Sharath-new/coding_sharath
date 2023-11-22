f4f6aa9eb0e41e7809fe73c8acd12c255af1ec77
Shashank-k12 <shashisandy2003@gmail.com>
2023-11-22 12:47:44 +0500

    what

---
+++ practice/first day/shashank_problems/problem5.js
@@ -0,0 +1,23 @@
+//A school has following rules for grading system:
+//a. Below 25 - F
+//b. 25 to 45 - E
+//c. 45 to 50 - D
+//d. 50 to 60 - C
+//e. 60 to 80 - B
+//f. Above 80 - A
+//Ask user to enter marks and print the corresponding grade.
+if(x < 25){
+    console.log("F")
+}else if(x <= 25 && x > 45){
+    console.log(E)
+}else if(x <= 45 && x > 50){
+    console.log("D")
+}else if(x <= 50 && x > 60){
+    console.log("c")
+}else if(x <= 60 && x > 80){
+    console.log("B")
+}else if(x > 80){
+    console.log("A")
+} 
+ //what is the error
+    
