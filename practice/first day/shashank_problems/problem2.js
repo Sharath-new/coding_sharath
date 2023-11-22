c4bcb3a8857469ac262a377ed754a7d7a1795994
Shashank-k12 <shashisandy2003@gmail.com>
2023-11-22 12:25:21 +0500

    problem 4

--- practice/first day/shashank_problems/problem2.js
+++ practice/first day/shashank_problems/problem2.js
@@ -1,21 +1,6 @@
-//to find the height of a person and cateogry according to their height
-
-
-// if ("height of a person is > 150") console.log("person is dwaf");
-// ifelse ("height of a person is = 150") console.log("person is average height");
-// else ("height of a person is > 150") console.log("person is taller")
-// if ("person height is <150") console.log("person is third preference")
-// else ("person height is = 150")console.log("person is second preference")
-// else("person height is > 150") console.log("person is first preference")
-
-// )
-
-var height = 160
-
-if (height > 150) {
-   
-} else if (height == 150) {
-   
-} else if (height < 150) {
-    
+var num = 18
+if(num > 18){
+console.log("person is not eligible to vote")
+}else {
+    console.log ("person is eligible for voting")
 }
